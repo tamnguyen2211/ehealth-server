6d748a70c7f21612bb318349169b6bdb8682b498
Phong Nguyen <nguyenphuphong@gmail.com>
2018-03-31 13:38:43 +0700

    * Init Back End API

---
+++ server.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import body_parser from 'body-parser';
+
+const mongo_client = require('mongodb').MongoClient;
+const uri = `mongodb://phongnguyen:P%40ssw0rd@ds231199.mlab.com:31199/eheath`;
+const app = express();
+
+app.use(body_parser.json());
+
+app.post('/medical-record', (req, res) => {
+    const medical_record = req.body;
+
+    mongo_client.connect(uri, (err, db) => {
+        if (err) throw err;
+
+        const dbo = db.db('eheath');
+        dbo.collection('medical-records').insertOne(medical_record, (err, data) => {
+            db.close();
+
+            if (err) throw err;
+
+            res.json(data);
+        });
+    });
+});
+
+app.listen(4000, () => {
+    console.log('Stock Analysis API Running on port 4000');
+});
