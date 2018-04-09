783d2b4eb1f2ec711b96576a2ea95fa36741e63b
nguyen.tam <thanhtam@bhdvn.com>
2018-04-09 18:54:55 +0700

    Update medical record by id

--- routes/medical_record.js
+++ routes/medical_record.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var request  = require('request');
 var db = require('../db')
+var ObjectID = require('mongodb').ObjectID;
 
 // get all medical record
 router.get('/', function(req,res){
@@ -30,5 +31,29 @@ router.post('/', function(req,res){
     });
 });
 
+
+router.post('/:id', function(req,res){
+    const id = req.params.id;
+    let newValue = req.body;
+    if(!newValue){
+        res.status(400).json({
+            message: "Missing body"
+        })
+        return;
+    }
+    var dbo = db.get().db('eheath');
+    var myquery = { _id: new ObjectID(id)};
+    newValue = {
+        $set: newValue
+    }
+    dbo.collection("medical-records").updateOne(myquery, newValue, function(err, result) {
+        if (err) throw err;
+        dbo.collection('medical-records').find(myquery).toArray(function(err, result) {
+            if (err) throw err;
+            res.json(result);
+        });
+        
+    });
+})
 //Return router
 module.exports = router;
