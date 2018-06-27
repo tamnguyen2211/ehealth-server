f08d102159cfb41193f999774db7516f783e9047
nguyen.tam <thanhtam@bhdvn.com>
2018-06-28 00:18:44 +0700

    update medical records

--- routes/patient.js
+++ routes/patient.js
@@ -125,7 +125,7 @@ router.get('/medical_records', function(req,res){
             })
             return;
         }
-        dbo.collection("medical_records").find({patient_id: patientId}).toArray((err, record) => {
+        dbo.collection("medical_records").find({patient_id: new ObjectID(patientId)}).toArray((err, record) => {
             if(err) throw err;
             if(!record){
                 res.status(404).json({
